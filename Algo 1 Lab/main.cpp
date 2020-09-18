b70b55e9ed98e3de16e04080f1f702c683653482
evilyash <evilyash02@gmail.com>
2020-09-18 23:23:02 +0530

    Initial Commit

---
+++ Algo 1 Lab/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algo 1 Lab
+//
+//  Created by Yash Agarwal on 9/18/20.
+//  Copyright © 2020 Yash Agarwal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
